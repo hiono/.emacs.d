b5057c3576e92d168b02f9187afcacdd2011390e
hiono <bps@sculd.com>
2022-06-06 13:08:17 +0900

    各種elispの導入

--- init/30_text-adjust.el
+++ init/30_text-adjust.el
@@ -5,10 +5,14 @@
     (when (memq major-mode '(org-mode text-mode wl-draft-mode))
       (text-adjust-space-buffer)))
   (defalias 'spacer 'text-adjust-space-buffer)
-  :hook (before-save-hook . text-adjust-space-before-save-if-needed)
-  :custom
-  (setq
-    adaptive-fill-regexp
-    "[ \t]*"
-    adaptive-fill-mode t
-    text-adjust-rule-kutouten text-adjust-rule-kutouten-zkuten))
+  (setq adaptive-fill-regexp "[ \t]*")
+  (setq adaptive-fill-mode t)
+  (setq text-adjust-rule-kutouten text-adjust-rule-kutouten-zkuten)
+  (setq text-adjust-rule-space
+    '
+    ((("[「」、。［］｛｝（）『』]" "" "\\ca") "")
+      (("\\ca" "" "[「」『』、。［］｛｝（）]") "")
+      (("\\cj\\|)" "" "[[(0-9a-zA-Z+]") " ")
+      (("[])/!?0-9a-zA-Z+]" "" "(\\|\\cj") " ")
+      (("]" " " "(") "")))
+  :hook (before-save-hook . text-adjust-space-before-save-if-needed))
