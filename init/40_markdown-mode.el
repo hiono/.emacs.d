d72f2a1166a76d3c5cd024372817061ad30b61bf
Hiroyuki Ono <hiroyuki.ono.jc@renesas.com>
2022-06-22 12:19:40 +0900

    markdown mode の修正

--- init/40_markdown-mode.el
+++ init/40_markdown-mode.el
@@ -3,6 +3,17 @@
   :init
   (use-package valign
     :straight t)
-  (add-hook 'markdown-mode-hook 'auto-fill-mode)
-  (add-hook 'markdown-mode-hook 'valign-mode)
-  :mode (("\\.markdown$" . markdown-mode) ("\\.md$" . markdown-mode)))
+  (use-package outline-magic
+    :straight t)
+  :hook
+  ((outline-mode-hook . (lambda () (require 'outline-cycle)))
+    (outline-minor-mode-hook
+      .
+      (lambda ()
+        (require 'outline-magic)
+        (define-key outline-minor-mode-map [C-c TAB] 'outline-cycle)))
+    (markdown-mode-hook . auto-fill-mode)
+    (markdown-mode-hook . valign-mode))
+  :mode
+  (("\\.markdown$" . markdown-mode)
+    ("\\.md$" . markdown-mode)))
