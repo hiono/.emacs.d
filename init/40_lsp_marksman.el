6f18501b98276dfcba8d91740cba9a13cc5a2ff5
hiono <bps@sculd.com>
2023-03-07 00:47:37 +0900

    Add LSP markdown

---
+++ init/40_lsp_marksman.el
@@ -0,0 +1,20 @@
+(use-package markdown-mode
+  :hook (markdown-mode . lsp)
+  :config
+  (require 'lsp-marksman)
+  :ensure-system-package (marksman . "sudo snap install --edge marksman"))
+
+;; (use-package
+;;   markdown-mode
+;;   :disabled
+;;   :init (use-package valign) (use-package outline-magic)
+;;   :hook
+;;   ((outline-mode-hook . (lambda () (require 'outline-cycle)))
+;;     (outline-minor-mode-hook
+;;       .
+;;       (lambda ()
+;;         (require 'outline-magic)
+;;         (define-key outline-minor-mode-map [C-c TAB] 'outline-cycle)))
+;;     (markdown-mode-hook . auto-fill-mode)
+;;     (markdown-mode-hook . valign-mode))
+;;   :mode (("\\.markdown$" . markdown-mode) ("\\.md$" . markdown-mode)))
